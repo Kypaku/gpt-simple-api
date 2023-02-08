1a04e65281b35843efd68f21573878dabfa5de8b
kypaku <kypaku@mail.ru>
2023-02-08 15:58:16 +0300

    to class

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import { Configuration, OpenAIApi, CreateCompletionRequest, CreateCompletionResponse } from "openai";
 
-export default {
-    key: "",
-    configuration: null as Configuration | null,
-    openai: null as OpenAIApi | null,
+export default class SimpleGPT {
+    protected _key: string
+    protected _configuration: Configuration | null
+    protected _openai: OpenAIApi | null
+    constructor ({key}: {key: string}) {
+        this._key = ""
+        this._configuration = null
+        this._openai = null
+        this.setApiKey(key)
+    }
     async get(promt: string, opts?: CreateCompletionRequest): Promise<null | string[]> {
-        if (!this.openai) return null;
-        const response = await this.openai.createCompletion({
+        if (!this._openai) return null;
+        const response = await this._openai.createCompletion({
             model: opts?.model || "text-davinci-003",
             prompt: promt || opts?.prompt,
             temperature: opts?.temperature || 0,
@@ -16,13 +22,13 @@ export default {
             presence_penalty: opts?.presence_penalty || 0,
         });
         return response.data.choices.map((choice) => choice.text).filter(Boolean) as string[];
-    },
+    }
     async getFirst(promt: string, opts?: CreateCompletionRequest): Promise<string | undefined> {
         return (await this.get(promt, opts))?.[0];
-    },
+    }
     async getCode(promt: string, opts?: CreateCompletionRequest): Promise<null | string[]> {
-        if (!this.openai) return null;
-        const response = await this.openai.createCompletion({
+        if (!this._openai) return null;
+        const response = await this._openai.createCompletion({
             model: opts?.model || "code-davinci-002",
             prompt: promt || opts?.prompt,
             temperature: opts?.temperature || 0,
@@ -35,12 +41,13 @@ export default {
     },
     async getCodeFirst(promt: string, opts?: CreateCompletionRequest): Promise<string | undefined> {
         return (await this.getCode(promt, opts))?.[0];
-    },
+    }
     setApiKey(key: string) {
-        this.key = key;
-        this.configuration = new Configuration({
-            apiKey: this.key,
+        this._key = key;
+        this._configuration = new Configuration({
+            apiKey: this._key,
         });
-        this.openai = new OpenAIApi(this.configuration);
-    },
-};
+        this._openai = new OpenAIApi(this._configuration);
+    }
+}
+
